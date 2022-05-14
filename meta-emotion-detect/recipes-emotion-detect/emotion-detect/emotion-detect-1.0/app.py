a66e7ddc7837174806245caab3ee57457f90f7f8
Chr1sus <arguedas951@gmail.com>
2022-05-13 19:07:13 -0600

    app incluida en el meta layer

--- meta-emotion-detect/recipes-emotion-detect/emotion-detect/emotion-detect-1.0/app.py
+++ meta-emotion-detect/recipes-emotion-detect/emotion-detect/emotion-detect-1.0/app.py
@@ -1,11 +1,11 @@
 import cv2
 import tensorflow as tf
 import numpy as np 
+import csv
 
 
 
-
-tflite_model_path = "/home/eladmin/yocto/poky/meta-emotion-detect/recipes-emotion-detect/emotion-detect/emotion-detect-1.0"
+tflite_model_path = "/usr/bin/em.tflite"
 interpreter = tf.lite.Interpreter(model_path=tflite_model_path)
 interpreter.allocate_tensors()
 input_details = interpreter.get_input_details()
@@ -13,12 +13,24 @@ output_details = interpreter.get_output_details()
 
 input_shape = input_details[0]['shape']
 
+fields = ['Enojo','Disgusto','Miedo'
+        ,'Feliz'
+        ,'Neutral'
+        ,'Triste'
+        ,'Sorprendido']
+filename = "records.csv"
+
+row  = [0,0,0
+        ,0
+        ,0
+        ,0
+        ,0]
 # Load image
 cv2.ocl.setUseOpenCL(False)
 
     # dictionary which assigns each label an emotion (alphabetical order)
 emotion_dict = {0: "Angry", 1: "Disgusted", 2: "Fearful", 3: "Happy", 4: "Neutral", 5: "Sad", 6: "Surprised"}
-
+aux=emotion_dict[4]
     # start the webcam feed
 cap = cv2.VideoCapture(0)
 while True:
@@ -26,7 +38,7 @@ while True:
     ret, frame = cap.read()
     if not ret:
         break
-    facecasc = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
+    facecasc = cv2.CascadeClassifier('/usr/bin/haarcascade_frontalface_default.xml')
     gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
     faces = facecasc.detectMultiScale(gray,scaleFactor=1.3, minNeighbors=5)
 
@@ -42,11 +54,22 @@ while True:
         output_data_tflite = interpreter.get_tensor(output_details[0]['index'])
 
         maxindex= int(np.argmax(output_data_tflite,axis=1))
+        if aux!=emotion_dict[maxindex]:
+            row[maxindex]+=1
+        aux=emotion_dict[maxindex]
         cv2.putText(frame, emotion_dict[maxindex], (x+20, y-60), cv2.FONT_HERSHEY_SIMPLEX, 1, (255, 255, 255), 2, cv2.LINE_AA)
 
     cv2.imshow('Video', cv2.resize(frame,(1600,960),interpolation = cv2.INTER_CUBIC))
     if cv2.waitKey(1) & 0xFF == ord('q'):
         break
 
+with open(filename, 'w') as csvfile:
+    csvwriter = csv.writer(csvfile)
+
+    # writing the fields
+    csvwriter.writerow(fields)
+
+    # writing the data rows
+    csvwriter.writerow(row)
 cap.release()
 cv2.destroyAllWindows()
